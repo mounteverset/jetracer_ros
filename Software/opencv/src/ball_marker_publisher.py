3e026a68f312c30e1b0526d249e6bb09e4430f5a
mounteverset <lukas.e@web.de>
2021-07-26 17:19:22 +0200

    goal scoring package revision

--- Software/opencv/src/ball_marker_publisher.py
+++ Software/opencv/src/ball_marker_publisher.py
@@ -7,6 +7,9 @@ from cv_bridge import CvBridge
 from geometry_msgs.msg import Point, Pose
 from sensor_msgs.msg import Image, CameraInfo
 from visualization_msgs.msg import Marker
+import tf, tf_conversions
+import tf2_ros
+import geometry_msgs.msg
 
 class BallDepthGetter():
 
@@ -27,12 +30,19 @@ class BallDepthGetter():
         self.marker.type = Marker.SPHERE
         self.marker.scale.x = 0.15
         self.marker.scale.y = 0.15
-        self.marker.scale.z = 0.15       
+        self.marker.scale.z = 0.15   
+        self.marker.pose.position.z = 0.075    
         self.marker.color.b = 0.0
         self.marker.color.r = 1.0
-        self.marker.color.g = 0.0
+        self.marker.color.g = 1.0
         self.marker.color.a = 1.0
         self.marker_pub = rospy.Publisher("visualization_marker", Marker, queue_size=5)
+
+        self.broadcaster = tf2_ros.TransformBroadcaster()
+        self.ball_transform = geometry_msgs.msg.TransformStamped()
+        self.ball_transform.header.frame_id = "camera_link"
+        self.ball_transform.child_frame_id = "ball"
+
 
         # self.position_tolerance = 0.1
         
@@ -48,6 +58,16 @@ class BallDepthGetter():
         self.blob_point.y = int(data.y)
         #rospy.loginfo("Got called in Blob Point Callback")
         rospy.loginfo("Blob Point x: {0} y:{1}".format(self.blob_point.x, self.blob_point.y))
+
+        angle = self.calculate_angle()
+
+        x_coord, y_coord = self.calculate_position(angle)
+
+        self.marker.pose.position.x = x_coord
+        self.marker.pose.position.y = y_coord
+
+        self.marker_pub.publish(self.marker)
+        self.broadcast_tf()
     
     # def camera_info_callback(self, data):
 
@@ -64,30 +84,33 @@ class BallDepthGetter():
         rospy.loginfo("Winkel: {0}".format(horizontal_angle))
         return horizontal_angle
 
+    def broadcast_tf(self):
+        self.ball_transform.header.stamp = rospy.Time.now()
+        self.ball_transform.transform.translation.x = self.marker.pose.position.x
+        self.ball_transform.transform.translation.y = self.marker.pose.position.y
+        q = tf_conversions.transformations.quaternion_from_euler(0 ,0, 0)
+        self.ball_transform.transform.rotation.x = q[0]
+        self.ball_transform.transform.rotation.y = q[1]
+        self.ball_transform.transform.rotation.z = q[2]
+        self.ball_transform.transform.rotation.w = q[3]
+
+        self.broadcaster.sendTransform(self.ball_transform)
+
+
     def calculate_position(self, angle):
-        x_coord = math.sin(math.radians(angle)) * self.depth
-        y_coord = math.cos(math.radians(angle)) * self.depth
+        x_coord = math.cos(math.radians(angle)) * self.depth
+        y_coord = - math.sin(math.radians(angle)) * self.depth
         rospy.loginfo("x: {0}, y: {1}".format(x_coord, y_coord))
-
-        self.marker.pose.position.x = x_coord
-        self.marker.pose.position.y = y_coord
-
-
-
-
+        return x_coord, y_coord
 
     def ball_depth_node(self):
         depth_image_sub = rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.depth_callback)
         blob_pixel_sub = rospy.Subscriber("/blob/point_blob", Point, self.blob_point_callback)
-        rate = rospy.Rate(2)
-        #camera_info_sub = rospy.Subscriber("/camera/depth/camera_info", CameraInfo, camera_info_callback)
-        while not rospy.is_shutdown():
-            
-            angle = self.calculate_angle()
-            self.calculate_position(angle)
-            self.marker_pub.publish(self.marker)
-            rate.sleep()
-        #rospy.spin()
+        # rate = rospy.Rate(2)
+        # camera_info_sub = rospy.Subscriber("/camera/depth/camera_info", CameraInfo, camera_info_callback)
+        # while not rospy.is_shutdown():
+        #     rate.sleep()
+        rospy.spin()
             # rospy.loginfo("Depth: {0}".format(self.depth))
             # rospy.loginfo("Blob Point x: {0} y:{1}".format(self.blob_point.x, self.blob_point.y))
 
