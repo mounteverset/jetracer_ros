7859e11e002ef848c8e24c1750246e6655190177
Lukas <s0570976@htw-berlin.de>
2021-07-26 17:52:13 +0200

    improved ball localization

---
+++ Software/goal_scoring/src/goal_scoring.py
@@ -0,0 +1,19 @@
+import rospy
+from geometry_msgs.msg import Point, Pose
+from visualization_msgs import Marker, MarkerArray
+import tf, tf_conversions
+import tf2_ros
+
+class Goal_Scoring():
+    def __init__(self):
+
+
+    
+    def run(self):
+        
+
+
+if __name__ == '__main__':
+    print("Starting...")
+    goal_scorer = Goal_Scoring()
+    goal_scorer.run()
