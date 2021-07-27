a84bd7cd81839a48bacfb8680f0aa6a4c797608d
Lukas <s0570976@htw-berlin.de>
2021-07-27 13:46:29 +0200

    Documentation

--- Software/opencv/src/goal_marker_publisher.py
+++ Software/opencv/src/goal_marker_publisher.py
@@ -14,49 +14,91 @@ class Goal_Publisher():
         self.marker_pub = rospy.Publisher("/visualization_marker", Marker, queue_size= 5)
         self.transform_listener = tf.TransformListener()
         self.marker = Marker()
+        self.marker.id = 0
         self.marker.header.frame_id = "map"
         self.marker.type = Marker.CUBE
         self.marker.scale.x = 0.15
         self.marker.scale.y = 0.75
         self.marker.scale.z = 0.35
         self.position_tolerance = 0.1
-        self.marker.color.b = 1.0
-        self.marker.color.r = 0.0
+        self.marker.color.b = 0.0
+        self.marker.color.r = 1.0
         self.marker.color.g = 0.0
         self.marker.color.a = 1.0
-        
+        self.broadcaster = tf.TransformBroadcaster()
 
     def goal_node(self):
-        trans = []
-        rot = []
+        
 
         while not rospy.is_shutdown():
+            trans = []
+            rot = []
             try:
                 self.transform_listener.waitForTransform("/map", "/object_6", rospy.Time(), rospy.Duration(2.0))
+                trans, rot = self.transform_listener.lookupTransform("/map", "/object_6",rospy.Time())
+                self.goal_detected = True
+                
+                if len(trans) > 0:   
+                    if abs(self.Pose.position.x - trans[0]) > self.position_tolerance or abs(self.Pose.position.y - trans[1]) > self.position_tolerance:
+                        self.Pose.position.x = trans[0]
+                        self.Pose.position.y = trans[1]
+                        self.Pose.position.z = 0.35 / 2
+                        self.Pose.orientation.x = 0.0 # rot[0]
+                        self.Pose.orientation.y = 0.0 # rot[1]
+                        self.Pose.orientation.z = rot[2] 
+                        self.Pose.orientation.w = rot[3]
+
+                        self.marker.pose = self.Pose
+                        rospy.sleep(1.0)
+                        self.marker_pub.publish(self.marker)
+
+                if self.goal_detected == True:
+                    # roll, pitch, yaw = tf.transformations.euler_from_quaternion([self.Pose.orientation.x, self.Pose.orientation.y, self.Pose.orientation.z, self.Pose.orientation.w])
+                    # roll = 0
+                    # pitch = 0
+                    # orientation
+                    self.broadcaster.sendTransform([self.Pose.position.x, self.Pose.position.y, 0.0], 
+                                                    [0.0, 0.0, self.Pose.orientation.z, self.Pose.orientation.w], 
+                                                    rospy.Time.now(), 
+                                                    "wilson_bag",
+                                                    "map")
+
             except:
                 print("Waiting for transform timeout.")
+                if self.goal_detected == True:
+                    # roll, pitch, yaw = tf.transformations.euler_from_quaternion([self.Pose.orientation.x, self.Pose.orientation.y, self.Pose.orientation.z, self.Pose.orientation.w])
+                    # roll = 0
+                    # pitch = 0
+                    # orientation
+                    self.broadcaster.sendTransform([self.Pose.position.x, self.Pose.position.y, 0.0], 
+                                                    [0.0, 0.0, self.Pose.orientation.z, self.Pose.orientation.w], 
+                                                    rospy.Time.now(), 
+                                                    "wilson_bag",
+                                                    "map")
 
-            try: 
-                trans, rot = self.transform_listener.lookupTransform("/map", "/object_6",rospy.Time())
-            except:
-                print("No transform.")
-                continue
+            # try: 
                 
+            # except:
+            #     print("No transform.")
+            #     continue
 
+            if len(trans) > 0:   
+                if abs(self.Pose.position.x - trans[0]) > self.position_tolerance or abs(self.Pose.position.y - trans[1]) > self.position_tolerance:
+                    self.Pose.position.x = trans[0]
+                    self.Pose.position.y = trans[1]
+                    self.Pose.position.z = 0.35 / 2
+                    self.Pose.orientation.x = 0.0 # rot[0]
+                    self.Pose.orientation.y = 0.0 # rot[1]
+                    self.Pose.orientation.z = rot[2] 
+                    self.Pose.orientation.w = rot[3]
 
-            if abs(self.Pose.position.x - trans[0]) > self.position_tolerance or abs(self.Pose.position.y - trans[1]) > self.position_tolerance:
-                self.Pose.position.x = trans[0]
-                self.Pose.position.y = trans[1]
-                self.Pose.position.z = 0.35 / 2
-                self.Pose.orientation.x = rot[0]
-                self.Pose.orientation.y = rot[1]
-                self.Pose.orientation.z = rot[2] 
-                self.Pose.orientation.w = rot[3]
-
-                self.marker.pose = self.Pose
-                rospy.sleep(1.0)
-                self.marker_pub.publish(self.marker)
+                    self.marker.pose = self.Pose
+                    rospy.sleep(1.0)
+                    self.marker_pub.publish(self.marker)
             
+            if self.goal_detected == True:
+                self.marker_pub.publish(self.marker)
+
             rospy.sleep(1.0)
 
             
